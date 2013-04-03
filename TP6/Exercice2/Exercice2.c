504237c253a1ce2ff0b647a249aa56edbbb7e121
BlisterBoy <mehdi.abelha@gmail.com>
2013-04-03 19:59:14 +0200

    Exo 2 Terminé

--- TP6/Exercice2/Exercice2.c
+++ TP6/Exercice2/Exercice2.c
@@ -3,15 +3,39 @@
 #include <string.h>
 
 
+//Q6
 void init_tab(int *tab, int n, int val){
-  int i;
   int* ptr = malloc(n*sizeof(int));
+  int i;
   for(i = 0; i < n; i++){
     ptr[i] = val;
   }
-  memcpy(tab, ptr, n*sizeof(int));}
+  memcpy(tab, ptr, n*sizeof(int));
+}
 
+//Q7
+void init_tab_bis(int *tab, int n, int val){
+  //En fait la fonction remplie bien le tableau mais pas avec la valeur entiere correspondante (elle le met en char)
+
+  //Peut on vraiment la coder avec memset ?
+  memset(tab, val, n*sizeof(int));
+  int i = 0;
+  for(i = 0; i < n; i++){
+    printf("%d\n", tab[i]);
+  }
+}
+
+void caddoc_bis(int *tab, int n){
+  memset(tab, 0, n*sizeof(int));
+}
+
+
+///MAIN
 main(){
+  //Q6
   int tab[10];
   init_tab(tab, 10, 5);
+
+  //Q6
+  init_tab_bis(tab, 10, 0);
 }
